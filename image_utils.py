53d6ad62e561a6f2717b0889d6d13011dc721522
nogashliz <nogashliz@gmail.com>
2026-01-05 12:33:02 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,9 +1,27 @@
 from PIL import Image
 import numpy as np
 from scipy.signal import convolve2d
+from skimage.filters import median
+from skimage.morphology import ball
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+    image = Image.open(path)
+    image = np.array(image)
+    return image
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+    grayscale_image = np.mean(image_array, axis=2)
+    kernelX = np.array([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ])
+    kernelY = np.array([
+        [ 1,  2,  1],
+        [ 0,  0,  0],
+        [-1, -2, -1]
+    ])
+    edgeX = convolve2d(grayscale_image, kernelX, mode='same', boundary='fill', fillvalue=0)
+    edgeY = convolve2d(grayscale_image, kernelY, mode='same', boundary='fill', fillvalue=0)
+    edgeMAG = np.sqrt(edgeX**2 + edgeY**2)
+    return edgeMAG
