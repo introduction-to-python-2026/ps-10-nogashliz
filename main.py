15707933d482ec6c9b282d8a0242949bf61dd1b5
nogashliz <nogashliz@gmail.com>
2026-01-05 12:39:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+mport numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from skimage.filters import median
+from skimage.morphology import ball
+from image_utils import load_image, edge_detection
 
+image_path = 'your_image.jpg' 
+image = load_image(image_path)
+clean_image = median(image, ball(3))
+edgeMAG = edge_detection(clean_image)
+threshold = 50 
+edge_binary = edgeMAG > threshold
+
+plt.figure(figsize=(10, 5))
+plt.imshow(edge_binary, cmap='gray')
+plt.title(f'Binary Edges (Threshold={threshold})')
+plt.axis('off')
+plt.show()
+
+edge_image = Image.fromarray((edge_binary * 255).astype(np.uint8))
+edge_image.save('my_edges.png')
